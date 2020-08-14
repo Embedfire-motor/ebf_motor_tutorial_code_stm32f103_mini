ae88fceee719439067fc9c6a235b335fb0c7ffc6
hejiawen <hjw0415@outlook.com>
2020-08-14 11:47:04 +0800

    步进电机和定时器基础部分完成

--- basis_part/TIM—高级定时器互补输出/User/led/bsp_led.h
+++ basis_part/TIM—高级定时器互补输出/User/led/bsp_led.h
@@ -5,20 +5,15 @@
 
 //引脚定义
 /*******************************************************/
-//R 红色灯
-#define LED1_PIN                  GPIO_PIN_5                
-#define LED1_GPIO_PORT            GPIOB                     
-#define LED1_GPIO_CLK_ENABLE()   __HAL_RCC_GPIOB_CLK_ENABLE()
 
-//G 绿色灯
-#define LED2_PIN                  GPIO_PIN_0               
-#define LED2_GPIO_PORT            GPIOB                      
-#define LED2_GPIO_CLK_ENABLE()   __HAL_RCC_GPIOB_CLK_ENABLE()
+#define LED1_PIN                  GPIO_PIN_2               
+#define LED1_GPIO_PORT            GPIOC                    
+#define LED1_GPIO_CLK_ENABLE()   __HAL_RCC_GPIOC_CLK_ENABLE()
 
-//B 蓝色灯
-#define LED3_PIN                  GPIO_PIN_1              
-#define LED3_GPIO_PORT            GPIOB                       
-#define LED3_GPIO_CLK_ENABLE()    __HAL_RCC_GPIOB_CLK_ENABLE()
+
+#define LED2_PIN                  GPIO_PIN_3              
+#define LED2_GPIO_PORT            GPIOC                      
+#define LED2_GPIO_CLK_ENABLE()   __HAL_RCC_GPIOC_CLK_ENABLE()
 
 /************************************************************/
 
@@ -27,19 +22,14 @@
 	* LED低电平亮，设置ON=0，OFF=1
 	* 若LED高电平亮，把宏设置成ON=1 ，OFF=0 即可
 	*/
-#define ON  GPIO_PIN_RESET
-#define OFF GPIO_PIN_SET
+//#define ON  GPIO_PIN_RESET
+//#define OFF GPIO_PIN_SET
 
 /* 带参宏，可以像内联函数一样使用 */
 #define LED1(a)	HAL_GPIO_WritePin(LED1_GPIO_PORT,LED1_PIN,a)
 
 
 #define LED2(a)	HAL_GPIO_WritePin(LED2_GPIO_PORT,LED2_PIN,a)
-
-
-#define LED3(a)	HAL_GPIO_WritePin(LED2_GPIO_PORT,LED3_PIN,a)
-
-
 
 
 /* 直接操作寄存器的方法控制IO */
@@ -57,65 +47,13 @@
 #define LED2_OFF		digitalHi(LED2_GPIO_PORT,LED2_PIN)
 #define LED2_ON			digitalLo(LED2_GPIO_PORT,LED2_PIN)
 
-#define LED3_TOGGLE		digitalToggle(LED3_GPIO_PORT,LED3_PIN)
-#define LED3_OFF		digitalHi(LED3_GPIO_PORT,LED3_PIN)
-#define LED3_ON			digitalLo(LED3_GPIO_PORT,LED3_PIN)
 
-
-
-/* 基本混色，后面高级用法使用PWM可混出全彩颜色,且效果更好 */
-
-//红
-#define LED_RED  \
-					LED1_ON;\
-					LED2_OFF\
-					LED3_OFF
-
-//绿
-#define LED_GREEN		\
-					LED1_OFF;\
-					LED2_ON\
-					LED3_OFF
-
-//蓝
-#define LED_BLUE	\
-					LED1_OFF;\
-					LED2_OFF\
-					LED3_ON
-
-					
-//黄(红+绿)					
-#define LED_YELLOW	\
-					LED1_ON;\
-					LED2_ON\
-					LED3_OFF
-//紫(红+蓝)
-#define LED_PURPLE	\
-					LED1_ON;\
-					LED2_OFF\
-					LED3_ON
-
-//青(绿+蓝)
-#define LED_CYAN \
-					LED1_OFF;\
-					LED2_ON\
-					LED3_ON
-					
-//白(红+绿+蓝)
-#define LED_WHITE	\
-					LED1_ON;\
-					LED2_ON\
-					LED3_ON
 					
 //黑(全部关闭)
 #define LED_RGBOFF	\
 					LED1_OFF;\
-					LED2_OFF\
-					LED3_OFF
+					LED2_OFF
 					
-
-
-
 
 void LED_GPIO_Config(void);
 
