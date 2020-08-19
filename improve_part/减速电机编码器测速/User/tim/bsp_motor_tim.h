774bdf20b39c6fb55ad9b855d667c536c79b1ad6
jc <798025823@qq.com>
2020-08-19 17:57:58 +0800

    提交有刷电机提高部分例程

---
+++ improve_part/减速电机编码器测速/User/tim/bsp_motor_tim.h
@@ -0,0 +1,42 @@
+#ifndef __BSP_MOTOR_TIM_H
+#define	__BSP_MOTOR_TIM_H
+
+#include "stm32f1xx.h"
+
+/*宏定义*/
+#define PWM_TIM                        	TIM4
+
+#define PWM_TIM_GPIO_AF_ENALBE()        //{__HAL_AFIO_REMAP_TIM3_PARTIAL();\
+																				__HAL_AFIO_REMAP_SWJ_NONJTRST();}
+
+#define PWM_TIM_CLK_ENABLE()  					__HAL_RCC_TIM4_CLK_ENABLE()
+
+#define PWM_CHANNEL_1                   TIM_CHANNEL_1
+#define PWM_CHANNEL_2                   TIM_CHANNEL_2
+
+/* 累计 TIM_Period个后产生一个更新或者中断*/		
+/* 当定时器从0计数到PWM_PERIOD_COUNT，即为PWM_PERIOD_COUNT+1次，为一个定时周期 */
+#define PWM_PERIOD_COUNT     (3600)
+
+/* 通用控制定时器时钟源TIMxCLK = HCLK=72MHz */
+/* 设定定时器频率为=TIMxCLK/(PWM_PRESCALER_COUNT+1) */
+#define PWM_PRESCALER_COUNT     (2)
+
+/* 最大比较值 */
+#define PWM_MAX_PERIOD_COUNT              (PWM_PERIOD_COUNT - 100)
+
+/*PWM引脚*/
+#define PWM_TIM_CH1_GPIO_CLK()             __HAL_RCC_GPIOB_CLK_ENABLE();
+#define PWM_TIM_CH1_GPIO_PORT              GPIOB
+#define PWM_TIM_CH1_PIN                    GPIO_PIN_6
+
+#define PWM_TIM_CH2_GPIO_CLK()             __HAL_RCC_GPIOB_CLK_ENABLE();
+#define PWM_TIM_CH2_GPIO_PORT              GPIOB
+#define PWM_TIM_CH2_PIN                    GPIO_PIN_7
+
+extern TIM_HandleTypeDef  TIM_TimeBaseStructure;
+
+extern void TIMx_Configuration(void);
+extern void TIM1_SetPWM_pulse(uint32_t channel,int compare);
+
+#endif   /* __BSP_MOTOR_TIM_H */
